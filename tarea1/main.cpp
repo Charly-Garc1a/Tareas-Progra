6464939ed15f74f06f3031827e951a5ca91367c8
Charly-Garc1a <cgarciag36@miumg.edu.gt>
2022-01-31 23:34:49 -0600

    Calculadora 1

---
+++ tarea1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout << "introduce el primer nœmero para las operaciones matem‡ticas: ";
+    cin >> num1;
+    cout << "introduce el primer nœmero para las operaciones matem‡ticas: ";
+    cin >> num2;
+    cout << "La suma de los numeros es: " << num1 + num2 << endl;
+    cout << "La resta de los numeros es: " << num1 - num2 << endl;
+    cout << "La multiplicaci—n de los numeros es: " << num1 * num2 << endl;
+    cout << "La divisi—n de los numeros es: " << num1 / num2 << endl;
+    return 0;
+}
