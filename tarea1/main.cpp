65b3bac945a7e21d48fbd17b173d2c4e7a613d5d
Charly-Garc1a <cgarciag36@miumg.edu.gt>
2022-02-07 17:36:46 -0600

    Subir tarea 2

--- tarea1/main.cpp
+++ tarea1/main.cpp
@@ -5,13 +5,14 @@ using namespace std;
 int main()
 {
     int num1, num2;
-    cout << "introduce el primer nœmero para las operaciones matem‡ticas: ";
+     cout << "************************CALCULADORA*************************"<< endl;
+    cout << "Introduce el primer numero para las operaciones matematicas: ";
     cin >> num1;
-    cout << "introduce el primer nœmero para las operaciones matem‡ticas: ";
+    cout << "Introduce el primer numero para las operaciones matematicas: ";
     cin >> num2;
     cout << "La suma de los numeros es: " << num1 + num2 << endl;
     cout << "La resta de los numeros es: " << num1 - num2 << endl;
-    cout << "La multiplicaci—n de los numeros es: " << num1 * num2 << endl;
-    cout << "La divisi—n de los numeros es: " << num1 / num2 << endl;
+    cout << "La multiplicacion de los numeros es: " << num1 * num2 << endl;
+    cout << "La division de los numeros es: " << num1 / num2 << endl;
     return 0;
 }
